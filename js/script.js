e4da43c3d629482bdac25826f513007e36df27b3
Hizkia Christian Meganathan <99702694+HizkiaCM@users.noreply.github.com>
2025-04-17 15:35:05 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ if (welcomeEl) {
         },
         {
             title: "Project 3",
-            description: "Description for project 2.",
+            description: "Description for project 3.",
             image: "assets/web des.png",
             link: "https://revou-fundamental-course.github.io/07-apr-25-HizkiaCM/"
         },
